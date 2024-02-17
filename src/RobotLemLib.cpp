031e697fca64069b60014b804cd1e9c9782fd8c0
Andy Dao <andydao2020@gmail.com>
2024-02-16 19:08:13 -0600

    LemLib Robot Addition

---
+++ src/RobotLemLib.cpp
@@ -0,0 +1,172 @@
+#include "RobotLemLib.h"
+
+void RobotLemLib::readConfig()
+{
+    std::ifstream file;
+    file.open("/usd/RobotLemLib.txt");
+
+    if (file.is_open())
+    {
+        for (int i = 0; i < 13; ++i)
+        {
+            configVector.push_back(std::make_pair(readPort(file), readReversed(file)));
+        }
+    }
+    else
+    {
+        primary_controller->updateDisplay(pros::E_CONTROLLER_MASTER, "Error reading config");
+    }
+}
+
+int RobotLemLib::readPort(std::ifstream &file)
+{
+    std::string portString = "";
+    std::getline(file, portString);
+    return stoi(portString);
+}
+
+bool RobotLemLib::readReversed(std::ifstream &file) {
+    std::string reversedString = "";
+    std::getline(file, reversedString);
+    int reversedInt = stoi(reversedString);
+    return reversedInt == 1;
+}
+
+// Initializes 18in robot
+void RobotLemLib::initialize()
+{
+    pros::lcd::initialize();
+    node_manager = new NodeManager(pros::millis);
+    primary_controller = new ControllerNode(node_manager, "primary");
+
+    readConfig();
+
+    /* Define the drivetrain components*/
+    pros::Motor left_drive_1(configVector[0].first, pros::E_MOTOR_GEARSET_18, configVector[0]) // Port 1, Gearset, Reverse-False
+    pros::Motor left_drive_2(configVector[1].first, pros::E_MOTOR_GEARSET_18, configVector[1]) // Port 1, Gearset, Reverse-False
+    pros::Motor left_drive_3(configVector[2].first, pros::E_MOTOR_GEARSET_18, configVector[2]) // Port 1, Gearset, Reverse-False
+    pros::Motor left_drive_4(configVector[3].first, pros::E_MOTOR_GEARSET_18, configVector[3]) // Port 1, Gearset, Reverse-False
+
+    pros::Motor right_drive_1(configVector[4].first, pros::E_MOTOR_GEARSET_18, configVector[4]) // Port 1, Gearset, Reverse-False
+    pros::Motor right_drive_2(configVector[5].first, pros::E_MOTOR_GEARSET_18, configVector[5]) // Port 1, Gearset, Reverse-False
+    pros::Motor right_drive_3(configVector[6].first, pros::E_MOTOR_GEARSET_18, configVector[6]) // Port 1, Gearset, Reverse-False
+    pros::Motor right_drive_4(configVector[7].first, pros::E_MOTOR_GEARSET_18, configVector[7]) // Port 1, Gearset, Reverse-False
+
+    pros::MotorGroup left_side_motors({left_drive_1, left_drive_2, left_drive_3, left_drive_4})
+    pros::MotorGroup right_side_motors({right_drive_1, right_drive_2, right_drive_3, right_drive_4})
+
+    lemlib::Drivetrain_t drivetrain {
+    &left_side_motors, // left drivetrain motors
+    &right_side_motors, // right drivetrain motors
+    10, // track width (in?)
+    3.35, // wheel diameter (in?)
+    360 // wheel rpm
+    };
+
+    /* Define Odometry Components */ //TODO Address this Odometry section
+    
+    // left tracking wheel encoder
+    pros::ADIEncoder left_enc('A', 'B', true); // ports A and B, reversed
+    // right tracking wheel encoder
+    pros::Rotation right_rot(1, false); // port 1, not reversed
+    // back tracking wheel encoder
+    pros::ADIEncoder back_enc('C', 'D', false); // ports C and D, not reversed
+    
+    // left tracking wheel
+    lemlib::TrackingWheel left_tracking_wheel(&left_enc, 2.75, -4.6); // 2.75" wheel diameter, -4.6" offset from tracking center
+    // right tracking wheel
+    lemlib::TrackingWheel right_tracking_wheel(&right_rot, 2.75, 1.7); // 2.75" wheel diameter, 1.7" offset from tracking center
+    lemlib::TrackingWheel back_tracking_wheel(&back_enc, 2.75, 4.5); // 2.75" wheel diameter, 4.5" offset from tracking center
+    
+    // inertial sensor
+    pros::Imu inertial_sensor(2); // port 2
+    
+    // odometry struct
+    lemlib::OdomSensors_t sensors {
+        &left_tracking_wheel, // vertical tracking wheel 1
+        &right_tracking_wheel, // vertical tracking wheel 2
+        &back_tracking_wheel, // horizontal tracking wheel 1
+        nullptr, // we don't have a second tracking wheel, so we set it to nullptr
+        &inertial_sensor // inertial sensor
+    };
+
+    /* Define PID Components */
+    
+    // Forward/Backward PID
+    lemlib::ChassisController_t lateralController {
+    8, // kP
+    30, // kD
+    1, // smallErrorRange
+    100, // smallErrorTimeout
+    3, // largeErrorRange
+    500, // largeErrorTimeout
+    5 // slew rate
+    };
+
+    // Turning PID
+    lemlib::ChassisController_t angularController {
+    4, // kP
+    40, // kD
+    1, // smallErrorRange
+    100, // smallErrorTimeout
+    3, // largeErrorRange
+    500, // largeErrorTimeout
+    0 // slew rate
+    };
+
+    // Create Chassis
+    lemlib::Chassis chassis(drivetrain, lateralController, angularController, sensors);
+
+    
+    /* Define the intake components */
+    intake_motor = new MotorNode(node_manager, configVector[8].first, "intake", configVector[8].second);
+    indexer_motor = new MotorNode(node_manager, configVector[9].first, "indexer", configVector[9].second);
+
+    intake_node = new IntakeNode(
+        node_manager, "intake", primary_controller,
+        pros::E_CONTROLLER_DIGITAL_A, pros::E_CONTROLLER_DIGITAL_B, {intake_motor});
+
+    indexer_node = new IntakeNode(
+        node_manager, "indexer", primary_controller,
+        pros::E_CONTROLLER_DIGITAL_A, pros::E_CONTROLLER_DIGITAL_B, {indexer_motor});
+
+    /* Define the Roller Components */
+    roller_motor = new MotorNode(node_manager, configVector[10].first, "roller", configVector[10].second);
+
+    roller_node = new IntakeNode(
+        node_manager, "roller", primary_controller,
+        pros::E_CONTROLLER_DIGITAL_L1, pros::E_CONTROLLER_DIGITAL_L2, {roller_motor});
+    
+    extra_motor_1 = new MotorNode(node_manager, configVector[11].first, "extra1", configVector[11].second);
+
+    extra_node_1 = new IntakeNode(
+        node_manager, "extra1", primary_controller,
+        pros::E_CONTROLLER_DIGITAL_L1, pros::E_CONTROLLER_DIGITAL_L2, {extra_motor_1});
+        
+    extra_motor_2 = new MotorNode(node_manager, configVector[12].first, "extra2", configVector[12].second);
+
+    extra_node_2 = new IntakeNode(
+        node_manager, "extra2", primary_controller,
+        pros::E_CONTROLLER_DIGITAL_X, pros::E_CONTROLLER_DIGITAL_Y, {extra_motor_2});
+}
+
+void RobotLemLib::disabled() {}
+
+void RobotLemLib::competition_initialize() {}
+
+void RobotLemLib::autonomous() {}
+
+// Must put all telepPeriodic() method from each class into here
+void RobotLemLib::opcontrol()
+{
+    while (true)
+    {
+        // nodeManager->executeTeleop();
+        tank_drive_node->teleopPeriodic();
+        intake_node->teleopPeriodic();
+        indexer_node->teleopPeriodic();
+        roller_node->teleopPeriodic();
+        extra_node_1->teleopPeriodic();
+        extra_node_2->teleopPeriodic();
+    }
+}
